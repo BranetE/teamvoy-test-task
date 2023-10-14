2c913492c8120730377a5baa57bce97b49fb3abe
Oleh Kulbaba <elatedexorcist@gmail.com>
2023-10-14 15:25:56 +0300

    Initial commit

---
+++ src/main/java/com/example/testtasktemvoy/TestTaskTemvoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtasktemvoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskTemvoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskTemvoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testtasktemvoy/TestTaskTemvoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtasktemvoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskTemvoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
