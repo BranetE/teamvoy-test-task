75f51a656178230245a859cf774a59dde54fb583
Oleh Kulbaba <elatedexorcist@gmail.com>
2023-10-19 13:18:54 +0300

    Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (email, first_name, last_name, password, role) VALUES ('user1@mail.com', 'User1', 'User1', '$2a$10$eO/1dvpivhrxVygU2ZgokeF3qriHigVAL81wwOk.wMnflbHIUNOOO', 'USER');
+INSERT INTO users (email, first_name, last_name, password, role) VALUES ('user2@mail.com', 'User2', 'User2', '$2a$10$AzSYv6J.wNyvBld3fmLRKOBEg5cy1TN7vgJgF664RSqdSwq27Zz8q', 'USER');
+INSERT INTO users (email, first_name, last_name, password, role) VALUES ('user3@mail.com', 'User3', 'User3', '$2a$10$swmCLGenUifCdF8QSWuonuTvW.4RbD5DHH6II9/iLJ2q11kcZYH3C', 'USER');
+INSERT INTO users (email, first_name, last_name, password, role) VALUES ('admin@mail.com', 'Admin', 'Admin', '$2a$10$KHs00ADCtnhzn0IiD2s9I.9JErVHbukYSJhdZM.RHQ/wfBwj/Sci2', 'ADMIN');
+
+INSERT INTO products (description, items_left, name, price) VALUES ('color: grey, Memory 512GB', 79, 'Samsung Galaxy S3', 900);
+INSERT INTO products (description, items_left, name, price) VALUES ('color: grey, Memory 1TB, Ram: 8Gg', 53, 'MacBook Air', 1100);
+INSERT INTO products (description, items_left, name, price) VALUES ('color: black, Memory 512Gb', 120, 'Google Pixel 7', 1000);
+INSERT INTO products (description, items_left, name, price) VALUES ('color: black, Memory 256Gb', 30, 'OnePlus 10 Pro', 700);
+INSERT INTO products (description, items_left, name, price) VALUES ('color: black, Memory 1TB', 150, 'IPhone 15', 1500);
+INSERT INTO products (description, items_left, name, price) VALUES ('color: black, Memory 1TB', 53, 'IPhone 13 Pro Max', 1100);
